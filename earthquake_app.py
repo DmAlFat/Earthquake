ed7102fe34c2e01161e20029605905ca4f1f2441
DmAlFat <fateev8292@gmail.com>
2022-07-17 19:53:01 +0300

    earthquake with db

--- earthquake_app.py
+++ earthquake_app.py
@@ -1,4 +1,25 @@
 import requests
+import sqlite3
+
+
+def save_eartquakes(place_magnitude_list):
+	conn = sqlite3.connect("earthquakes_db.db")
+	cursor = conn.cursor()
+	cursor.execute("CREATE TABLE earthquakes (place TEXT, magnitude REAL)")
+	cursor.executemany("INSERT INTO earthquakes VALUES (?, ?)", place_magnitude_list)
+	conn.commit()
+	conn.close()
+
+
+def select_all_earthquakes():
+	conn = sqlite3.connect("earthquakes_db.db")
+	cursor = conn.cursor()
+	cursor.execute("SELECT * FROM earthquakes")
+	data = cursor.fetchall()
+	[print(row) for row in data]
+	conn.commit()
+	conn.close()
+
 
 url = 'https://earthquake.usgs.gov/fdsnws/event/1/query?'
 
@@ -23,7 +44,11 @@ response = requests.get(url, headers={'Accept':'application/json'}, params={
 data = response.json()
 
 earthquake_list = data['features']
-count = 0
+place_magnitude_list = []
+
 for earthquake in earthquake_list:
-	count += 1
-	print(f"{count}. Place: {earthquake['properties']['place']}. Magnitude: {earthquake['properties']['mag']}.")
+	place_magnitude_list.append((earthquake['properties']['place'], earthquake['properties']['mag']))
+
+save_eartquakes(place_magnitude_list)
+
+select_all_earthquakes()
